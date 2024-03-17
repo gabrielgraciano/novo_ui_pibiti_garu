b2f4867d4fbc656e22d91e9b6bbe4ae8dd09f162
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2024-03-17 14:48:57 -0300

    Update ui.R
    Retirei probabilidade e inferência

--- pibiti_garu_estatistica/ui.R
+++ pibiti_garu_estatistica/ui.R
@@ -35,7 +35,6 @@ useShinyalert(force=TRUE)
 #substitui inferencia por outras quatro sources (ainda não troquei, talvez não precise)
 
 
-
 dashboardPage(
   dashboardHeader(title = 'Garu Estatística'),
   dashboardSidebar(
@@ -52,16 +51,6 @@ dashboardPage(
                menuSubItem('Variáveis Quantitativas', tabName = 'graf_quantitativa'),
                menuSubItem('Gráficos Bidimensionais', tabName = 'graf_bidimensional')),
       
-      menuItem('Probabilidade', icon = icon("percent", lib="font-awesome"),
-               menuSubItem('Definição de Probabilidade', tabName = 'def_prob'),
-               menuSubItem('Probabilidade Condicional', tabName = 'prob_cond'),
-               menuSubItem('Distribuições de Probabilidade', tabName = 'distr_prob')),
-      
-      menuItem('Inferência', icon = icon("area-chart", lib="font-awesome"),
-               menuSubItem('Teste T para uma amostra', tabName = 'teste_t_1'),
-               menuSubItem('Teste T para duas amostras (dep.)', tabName = 'teste_t_2'),
-               menuSubItem('Teste qui quadrado', tabName = 'teste_qui'),
-               menuSubItem('Teste de Correlação', tabName = 'teste_corr')),
       
       menuItem('Glossário', tabName = 'glossario', icon = icon("font", lib = "font-awesome")),
                
@@ -95,13 +84,6 @@ dashboardPage(
       graf_quantitativa,
       graf_qualitativa,
       graf_bidimensional,
-      def_prob,
-      prob_cond,
-      distr_prob,
-      teste_t_1,
-      teste_t_2,
-      teste_qui,
-      teste_corr,
       glossario,
       questionario,
       paralisia
@@ -109,13 +91,3 @@ dashboardPage(
   )
 )
 
-
-
-
-
-#navbarMenu("Glosssário",
-#tabPanel('Glossário', glossario, value = 'tabGlossario'),
-#icon = icon('font', lib='font-awesome')),
-
-
-#tabPanel('Glossário', glossario, value = 'tabGlossario'),
